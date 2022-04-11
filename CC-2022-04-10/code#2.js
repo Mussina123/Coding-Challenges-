44509205d46b6fdcaf82c0e29d0c400db0f20036
Tyler Beck <softwaretpd2035@gmail.com>
2022-04-10 20:36:06 -0400

    Coding Challenge #2 04-10-2022

---
+++ CC-2022-04-10/code#2.js
@@ -0,0 +1,7 @@
+// Name Shuffler 
+
+// Write a function that returns a string in which firstname is swapped with last name.
+
+// nameShuffler('john McClane'); => "McClane john"
+
+let nameShuffler = str => str.split(' ').reverse().join(' ')
