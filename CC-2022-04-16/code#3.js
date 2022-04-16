46a2a6271a97b5c1f2f2403e43d85ca0dbee7871
Tyler Beck <softwaretpd2035@gmail.com>
2022-04-16 14:25:34 -0400

    Coding Challenge #3 04-16-2022

---
+++ CC-2022-04-16/code#3.js
@@ -0,0 +1,20 @@
+// Double Char 
+
+// Given a string, you have to return a string in which each character (case-sensitive) is repeated once.
+
+// Examples (Input -> Output):
+// * "String"      -> "SSttrriinngg"
+// * "Hello World" -> "HHeelllloo  WWoorrlldd"
+// * "1234!_ "     -> "11223344!!__  "
+// Good Luck!
+
+
+function doubleChar(str) {
+    // Your code here
+    let final_str = ''
+    let str_array = str.split('')
+    str_array.forEach(letter => {
+      final_str += letter + letter
+    })
+    return final_str
+  }
